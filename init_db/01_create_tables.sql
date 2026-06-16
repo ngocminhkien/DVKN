234a320db2248f9c8aab707091ec5d9bd8d4377d
Ngoc Minh Kien <nkien1231@gmail.com>
2026-06-16 20:19:40 +0700

    add

---
+++ init_db/01_create_tables.sql
@@ -0,0 +1,27 @@
+-- Bảng chứa dữ liệu cảm biến IoT (Từ nhóm B1)
+CREATE TABLE IF NOT EXISTS sensor_events (
+    time TIMESTAMPTZ NOT NULL,
+    event_id VARCHAR(100) NOT NULL,
+    device_id VARCHAR(100) NOT NULL,
+    temperature_c DOUBLE PRECISION,
+    humidity_percent DOUBLE PRECISION,
+    co2_ppm DOUBLE PRECISION,
+    status VARCHAR(50),
+    alert_level VARCHAR(50),
+    reason VARCHAR(255),
+    raw_payload JSONB -- Lưu trữ toàn bộ cục JSON gốc phòng khi cần
+);
+
+-- Chuyển thành siêu bảng (Hypertable) của TimescaleDB để tối ưu truy vấn thời gian
+SELECT create_hypertable('sensor_events', 'time', if_not_exists => TRUE);
+
+
+-- Bảng chứa dữ liệu sự kiện quẹt thẻ và báo cháy (Từ nhóm B6)
+CREATE TABLE IF NOT EXISTS campus_logs (
+    time TIMESTAMPTZ NOT NULL,
+    log_type VARCHAR(50) NOT NULL,
+    details JSONB NOT NULL -- Sử dụng JSONB vì bên trong detail của Access và Fire Alarm có các trường khác nhau
+);
+
+-- Chuyển thành siêu bảng (Hypertable)
+SELECT create_hypertable('campus_logs', 'time', if_not_exists => TRUE);
